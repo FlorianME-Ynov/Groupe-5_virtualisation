f15e0d81ad7c6a123f9e1cb8c37a7edba9eec389
FlorianME-Ynov <72863224+FlorianME-Ynov@users.noreply.github.com>
2021-03-18 12:51:20 +0100

    Create script_2.sh

---
+++ script_2.sh
@@ -0,0 +1,70 @@
+#! /bin/bash
+echo "--- Multimedia Server ---"
+echo ""
+echo "Installation et Reload"
+echo "<1> 	Installation Complète"
+echo "<2> 	Recharger la configuration initiale"
+echo ""
+echo "Gestion de containers"
+echo "<3> 	Lancer les containers"
+echo "<4> 	Arreter les containers"cd ..
+echo "<5> 	Relancer les containers"
+echo ""
+echo "Gestion et mise à jour"
+echo "<6>   Lancer la supervision"
+echo ""
+echo "Autres Options"
+echo "<q>	q = Quitter"
+read choix 
+case $choix in 
+  1) sudo apt-get -y install docker docker-compose
+    sudo docker-compose down
+    mkdir multimedia-project
+    cd multimedia-project
+    mkdir -p {jacket,jellyfin,radarr,sonarr,transmission,torrents,videos,cloudfare,bazarr,nzbhydra2,organizr,heimdall}
+    cd ..
+    tar -xf ./Configs -C ./
+    sudo docker-compose up -d
+    sudo docker run -d \
+      -p 8000:8000 \
+      -p 9000:9000 \
+      --name=portainer \
+      --restart=always \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      -v portainer_data:/data portainer/portainer-ce 
+
+    sudo docker run -d \
+      --name watchtower \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      containrrr/watchtower
+
+  ;;
+  2) sudo tar -xvf ./Configs -C ./
+  ;;
+
+  3) sudo docker-compose up -d
+  ;;
+
+  4) sudo docker-compose down
+  ;;
+
+  5) sudo docker-compose down
+     sudo docker-compose up -d
+  ;;
+
+  6) sudo docker run -d \
+      -p 8000:8000 \
+      -p 9000:9000 \
+      --name=portainer \
+      --restart=always \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      -v portainer_data:/data portainer/portainer-ce 
+
+    sudo docker run -d \
+      --name watchtower \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      containrrr/watchtower
+  ;;
+  
+  q) exit;;
+esac
