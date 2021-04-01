fa6821e2ad52a72a2fbcf8a12ca798d08ddbb290
FlorianME-Ynov <72863224+FlorianME-Ynov@users.noreply.github.com>
2021-04-01 10:32:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
 wget https://raw.githubusercontent.com/FlorianME-Ynov/Groupe-5_virtualisation/main/script.sh
-curl https://raw.githubusercontent.com/FlorianME-Ynov/Groupe-5_virtualisation/main/docker-compose.yaml --output ./docker-compose.yaml
-wget https://github.com/FlorianME-Ynov/Groupe-5_virtualisation/raw/main/Configs
-
 chmod 777 script.sh
 sudo ./script.sh
