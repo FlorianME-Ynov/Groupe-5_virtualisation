e340cc3b7ebc7c9fd3ae6f77b76e0b6d6751b66b
FlorianME-Ynov <72863224+FlorianME-Ynov@users.noreply.github.com>
2021-04-01 11:51:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-wget https://raw.githubusercontent.com/FlorianME-Ynov/Groupe-5_virtualisation/main/install.sh
+wget https://raw.githubusercontent.com/FlorianME-Ynov/Groupe-5_virtualisation/main/script.sh
 chmod 777 ./script.sh
 sudo ./script.sh
